549a77cce28ea264ddf530e21922c4a95462e84b
finneen.yao <finneen.yao@gmail.com>
2014-12-11 10:11:27 +0800

    删除多余文件

---
+++ finneen-web/src/test/java/com/finneen/web/test/TestShiro.java
@@ -0,0 +1,17 @@
+package com.finneen.web.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yaofeng on 2014/12/11.
+ */
+public class TestShiro {
+    
+    static Logger logger = LoggerFactory.getLogger(TestShiro.class);
+    
+	public static void main(String[] args) {
+	
+	    
+	}
+}
